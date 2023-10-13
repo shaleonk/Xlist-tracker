e0e2e04e0f7bda235aaca48e10f5bc88bfe92f42
shaleonk <x93266264@gmail.com>
2023-10-14 06:34:10 +0800

    first-info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
-import HEro From "@components/Hero"
+import Hero from '../components/Hero';
+import Navbar from '../components/navbar';
+import Features from '../components/Features';
+import Testimoinals from '../components/Testimoinals';
+import Pricing from '../components/pricing';
+import Footer from '../components/Footer';
+import Newsletter from '../components/Newsletter';
+import Butt from '../components/Button';
 
 export default function Home() {
-    return (
-        <>
-            <Hero />
-        </>
-    )
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Testimoinals />
+      <Pricing />
+      <Newsletter />
+      <Footer />
+
+
+    </div>
+  );
+
 }
