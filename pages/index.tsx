7a115ee49e098c1bf64eae3d8d0d27c517010dc5
shaleonk <x93266264@gmail.com>
2023-10-15 08:14:22 +0800

    Pricing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Hero from '../components/Hero';
 import Navbar from '../components/navbar';
 import Features from '../components/Features';
 import Testimoinals from '../components/Testimoinals';
-import Pricing from '../components/pricing';
+import Pricing from '../components/Pricing';
 import Footer from '../components/Footer';
 import Newsletter from '../components/Newsletter';
 import Butt from '../components/Button';
@@ -17,7 +17,6 @@ export default function Home() {
       <Pricing />
       <Newsletter />
       <Footer />
-
 
     </div>
   );
